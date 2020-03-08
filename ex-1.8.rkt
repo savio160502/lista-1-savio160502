1e55636ccea88906f858db33a202711ee012eea1
savio <savio.vinicius00@gmail.com>
2020-03-08 18:40:28 -0300

    exercicio

--- ex-1.8.rkt
+++ ex-1.8.rkt
@@ -4,23 +4,25 @@
   (if (< x 0) (- x) x))
 
 (define (average x y)
-  (/ (+ x y) 2))
+  (/ (+ x y) 3))
 
 (define (square b)
   (* b b))
+(define (cube b) (* b b b))
 
 (define (cube-root x)
   (define (good-enough? guess)
     (< (abs (- (square guess) x)) 0.00001))
 
-  (define (good-enough2? ...)
-    ...)
+  (define (good-enough2? guess)
+     (< (abs (- (cube guess) x)) 0.00001))
 
-  (define (improve guess)
-    ...)
+  (define (improve guess x) 
+               (average (/ x (square guess)) (* 2 guess))) 
 
   (define (aux guess)
-    ...)
+    (if (good-enough2? guess) guess
+        (aux (improve guess x))))
 
   (aux 1.0))
 
